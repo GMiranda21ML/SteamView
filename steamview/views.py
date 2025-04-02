14198ade162dd9afaf1365ef8e0f3be0b1a214aa
GMiranda21ML <gabriel.mucarbel.lima@gmail.com>
2025-04-02 12:31:55 -0300

    ajeitando a view da pagina inicial dos jogos, começando criando uma função de buscar o jogo pelo nome usando a api da rawg

--- steamview/views.py
+++ steamview/views.py
@@ -1,43 +1,18 @@
 from django.shortcuts import render
+from bs4 import BeautifulSoup  
 import requests
 
-def paginaJogo(request):
-    game_name = "dark souls"
+API_KEY = "2965d09ddf6e4c47ad963c0a15e4e7db"  
 
-    url_all_games = "https://api.steampowered.com/ISteamApps/GetAppList/v2/"
-    response = requests.get(url_all_games)
+# Buscando o jogo pelo nome usando a API da RAWG e retorna seu ID.
+def buscar_jogo_por_nome(game_name):
+    url = f"https://api.rawg.io/api/games?key={API_KEY}&search={game_name}"
+    response = requests.get(url)
 
     if response.status_code == 200:
-        games = response.json()["applist"]["apps"]
-    else:
-        games = None
+        results = response.json().get("results", [])
+        if results:
+            return results[0]  
+    return None
 
-    appId = None
-    if games:
-        for game in games:
-            if game_name.lower() in game["name"].lower():
-                appId = game["appid"]
-                break
 
-    gameInfo = None
-    if appId:
-        url_game_details = f"https://store.steampowered.com/api/appdetails?appids={appId}"
-        response = requests.get(url_game_details)
-
-        if response.status_code == 200:
-            data = response.json()
-            if data[str(appId)]["success"]:
-                game_details = data[str(appId)]["data"]
-
-                gameInfo = {
-                    "name": game_details.get("name", "N/A"),
-                    "description": game_details.get("short_description", "Descrição não disponível."),
-                    "price": game_details.get("price_overview", {}).get("final_formatted", "Preço não disponível"),
-                    "image_url": game_details.get("header_image", "Imagem não disponível")
-                }
-
-    context = {
-        "gameInfo": gameInfo
-    }
-
-    return render(request, "paginaJogo.html", context)
