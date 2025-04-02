7f2370251b2f3e0591e8517f24a31553b1ff1247
GMiranda21ML <gabriel.mucarbel.lima@gmail.com>
2025-04-02 12:33:18 -0300

    criando uma função para buscar os detalhes do jogo

--- steamview/views.py
+++ steamview/views.py
@@ -5,7 +5,7 @@ import requests
 API_KEY = "2965d09ddf6e4c47ad963c0a15e4e7db"  
 
 # Buscando o jogo pelo nome usando a API da RAWG e retorna seu ID.
-def buscar_jogo_por_nome(game_name):
+def buscarJogoPorNome(game_name):
     url = f"https://api.rawg.io/api/games?key={API_KEY}&search={game_name}"
     response = requests.get(url)
 
@@ -13,6 +13,16 @@ def buscar_jogo_por_nome(game_name):
         results = response.json().get("results", [])
         if results:
             return results[0]  
+    return None
+
+
+# Busca os detalhes completos do jogo usando o ID do jogo.
+def buscarDetalhesDoJogo(game_id):
+    url = f"https://api.rawg.io/api/games/{game_id}?key={API_KEY}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return response.json()  
     return None
 
 
