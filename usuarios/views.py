9cad224b638bdc5db5e3323e71d8752d5360ed50
GMiranda21ML <gabriel.mucarbel.lima@gmail.com>
2025-04-03 18:14:39 -0300

    criando a view do cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -4,5 +4,27 @@ from django.contrib import messages
 
 # Create your views here.
 
-def cadastro(request):        
+def cadastro(request):    
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password1 = request.POST["password1"]
+        password2 = request.POST["password2"]
+
+        if password1 != password2:
+            messages.error(request, "As senhas não coincidem")
+        elif User.objects.filter(username = username).exists():
+            messages.error(request, "Nome de usuário já existe")
+        elif User.objects.filter(email = email).exists():
+            messages.error(request, "E-mail já cadastrado")
+        else:
+            user = User.objects.create_user(
+                username = username, 
+                email = email, 
+                password = password1
+            )
+            user.save()
+            messages.success(request, "Cadastro reealizado com sucesso!")
+            return redirect("login") 
+           
     return render(request, "usuarios/cadastro.html")
