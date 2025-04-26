d701138b4d84c9697482472985e897ee1bea840e
CaioMathews <cmff@cesar.school>
2025-04-25 22:01:09 -0300

    implementando Django na tela dos mais jogados

--- steamview/urls.py
+++ steamview/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from steamview.views import paginaJogo, searchBar, api_jogos, ratingSearchPage,lancamentos
+from steamview.views import paginaJogo, searchBar, api_jogos, ratingSearchPage,lancamentos, maisJogados
 urlpatterns = [
     path("", searchBar, name ="searchBar"),
     path("ratingsearch/api/", api_jogos, name="api_jogos"),
     path("ratingsearch/", ratingSearchPage, name="ratingSearchPage"),
     path("jogo/<str:nome>/", paginaJogo, name="paginaJogo"),
     path("lancamentos/", lancamentos, name="lancamentos"),
+    path("maisjogados/", maisJogados, name="maisJogados"),
 ]
